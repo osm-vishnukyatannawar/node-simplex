33c985cc30c9a6643e87116cf68f6662296b0e01
Abijeet <abijeetpatro@gmail.com>
2014-11-11 22:47:40 +0530

    Updating config.js

--- app/config.js
+++ app/config.js
@@ -3,7 +3,8 @@ var PRODUCTION = process.env.NODE_ENV === "production";
 global.__CONFIG__ = {
   'app_base_path': __dirname + '/',
   'app_code_path' : __dirname + '/code/',
-  'app_base_url': '/api/v1/'
+  'app_base_url': '/api/v1/',
+  'app_base_url_token' : '/api/v1/:token/'
 };
 
 config.express = {
