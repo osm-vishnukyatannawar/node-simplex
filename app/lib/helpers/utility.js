abd935392ae2de4bf6fa0677449344c81797b6ce
Abijeet <abijeet.p@osmosys.asia>
2014-11-13 00:39:07 +0530

    Added method to add hours to date

--- app/lib/helpers/utility.js
+++ app/lib/helpers/utility.js
@@ -1,6 +1,6 @@
 var util = {
   toMySQLDateTime: function(dt) {
-    if (dt === undefined) {
+    if (!dt) {
       dt = new Date();
     }
     return dt.toISOString().slice(0, 19).replace('T', ' ');
@@ -13,7 +13,14 @@ var util = {
 
     //when t[3], t[4] and t[5] are missing they defaults to zero
     return new Date(t[0], t[1] - 1, t[2], t[3] || 0, t[4] || 0, t[5] || 0);
-  } 
+  },
+  addHoursToDate : function(dt, hoursToAdd) {
+    if(!dt) {
+      dt = new Date();
+    }    
+    dt.setHours(dt.getHours() + hoursToAdd);
+    return dt;
+  }
 };
 
 module.exports = util;
