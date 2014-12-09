ec1bed708938111f8073bad3823f21bfd46a7f3c
Abijeet <abijeet.p@osmosys.asia>
2014-12-09 16:34:30 +0530

    Modifying server, adding a helper.

---
+++ app/lib/server-helper.js
@@ -0,0 +1,82 @@
+var __ = require('underscore');
+
+var serverHelper = function() {
+  var app = null;
+
+  var init = function(baseApp) {
+    app = baseApp;
+    app.httpPost = function(url, route, isPublic) {
+      bindHttpRequest(getFinalUrl(url, isPublic), route, isPublic, 'post');
+    };
+
+    app.httpGet = function(url, route, isPublic) {
+      bindHttpRequest(getFinalUrl(url, isPublic), route, isPublic, 'get');
+    };
+
+    app.httpDelete = function(url, route, isPublic) {
+      bindHttpRequest(getFinalUrl(url, isPublic), route, isPublic, 'delete');
+    };
+  };
+
+  var bindHttpRequest = function(url, route, isPublic, method) {
+    if (isPublic) {
+      app[method](url, route);
+    } else {
+      app[method](url, loadApi.validate);
+      app[method](url, route);
+    }
+  };
+
+  var normalizeUrl = function(url) {
+    if (url.indexOf('/') === 0) { return url; }
+    return url;
+  };
+
+  var getFinalUrl = function(url, isPublic) {
+    url = normalizeUrl(url);
+    var finalUrl = __CONFIG__.app_base_url_token + url;
+    if (isPublic) {
+      finalUrl = __CONFIG__.app_base_url + url;
+    }
+    return finalUrl;
+  };
+
+  var parseQueryStringValues = function(request, response, next) {
+    if (__.isEmpty(request.query)) {
+      request.queryParams = {};
+      next();
+      return;
+    }
+    request.queryParams = {};
+    request.queryParams.limit = parseInt(request.query.limit, 10);
+    request.queryParams.startRecord = parseInt(request.query.startRecord, 10);
+
+    request.queryParams.limit = isNaN(request.queryParams.limit) ? 20
+            : request.queryParams.limit;
+
+    request.queryParams.startRecord = isNaN(request.queryParams.startRecord)
+            ? 0 : request.queryParams.startRecord;
+
+    // Checking if it's a string and check that it's not empty
+    request.queryParams.search = (__.isString(request.query.q)
+            && !__.isEmpty(request.query.q) ? request.query.q : false);
+
+    request.queryParams.seachCol = (__.isString(request.query.qcol)
+            && !__.isEmpty(request.query.qcol) ? request.query.qcol : '*');
+
+    request.queryParams.sortBy = (__.isString(request.query.sortby)
+            && !__.isEmpty(request.query.sortby) ? request.query.sortby : false);
+
+    request.queryParams.sortCol = (__.isString(request.query.sortcol)
+            && !__.isEmpty(request.query.sortcol) ? request.query.sortcol
+            : false);
+    next();
+  };
+
+  return {
+    parseQueryString: parseQueryStringValues,
+    init: init,
+  };
+};
+
+module.exports = serverHelper();
--- app/server.js
+++ app/server.js
@@ -6,80 +6,17 @@ var bodyParser = require('body-parser');
 var __ = require('underscore');
 
 // Osm Includes
-var config = require("./config");
-var logger = require("./logger");
-var middleware = require("./middleware/index");
-var loadViews = require("./code/views.js");
-var loadApi = require("./code/api.js");
-
-var express = require("express");
+var config = require('./config');
+var logger = require('./logger');
+var middleware = require('./middleware/index');
+var loadViews = require('./code/views.js');
+var loadApi = require('./code/api.js');
+var getStatus = require('./lib/status');
+var express = require('express');
 var app = express();
 
-app.httpPost = function(url, route, isPublic) {  
-  bindHttpRequest(getFinalUrl(url, isPublic), route, isPublic, 'post');
-};
-
-app.httpGet = function(url, route, isPublic) {  
-  bindHttpRequest(getFinalUrl(url, isPublic), route, isPublic, 'get');
-};
-
-app.httpDelete = function(url, route, isPublic) {
-  bindHttpRequest(getFinalUrl(url, isPublic), route, isPublic, 'delete');
-};
-
-function bindHttpRequest(url, route, isPublic, method) {
-  if (isPublic) {
-    app[method](url, route);
-  } else {
-    app[method](url, loadApi.validate);
-    app[method](url, route);
-  }
-}
-
-function normalizeUrl(url) {
-  if (url.indexOf('/') === 0) { return url; }
-  return url;
-}
-
-function getFinalUrl(url, isPublic) {
-  url = normalizeUrl(url);
-  var finalUrl = __CONFIG__.app_base_url_token + url;
-  if (isPublic) {
-    finalUrl = __CONFIG__.app_base_url + url;
-  }
-  return finalUrl;
-}
-
-function parseQueryStringValues(request, response, next) {
-  if (__.isEmpty(request.query)) {
-    request.queryParams = {};
-    next();
-    return;
-  }
-  request.queryParams = {};
-  request.queryParams.limit = parseInt(request.query.limit, 10);
-  request.queryParams.startRecord = parseInt(request.query.startRecord, 10);
-
-  request.queryParams.limit = isNaN(request.queryParams.limit) ? 20
-          : request.queryParams.limit;
-
-  request.queryParams.startRecord = isNaN(request.queryParams.startRecord) ? 0
-          : request.queryParams.startRecord;
-
-  // Checking if it's a string and check that it's not empty
-  request.queryParams.search = (__.isString(request.query.q)
-          && !__.isEmpty(request.query.q) ? request.query.q : false);
-
-  request.queryParams.seachCol = (__.isString(request.query.qcol)
-          && !__.isEmpty(request.query.qcol) ? request.query.qcol : '*');
-
-  request.queryParams.sortBy = (__.isString(request.query.sortby)
-          && !__.isEmpty(request.query.sortby) ? request.query.sortby : false);
-
-  request.queryParams.sortCol = (__.isString(request.query.sortcol)
-          && !__.isEmpty(request.query.sortcol) ? request.query.sortcol : false);
-  next();
-}
+var helper = require('./lib/server-helper');
+helper.init(app);
 
 // The master process - will only be used when on PROD
 if (config.express.isProduction && cluster.isMaster) {
@@ -93,7 +30,20 @@ if (config.express.isProduction && cluster.isMaster) {
 } else {
   // A worker process
   app.use(bodyParser.json());
-  app.use(parseQueryStringValues);
+  
+  app.use(function(err, req, res, next) {
+    res.setHeader('X-Powered-By', 'Emanate Wireless');
+    if(err) {
+      res.status(getStatus('badRequest')).json({
+        status : 'fail',
+        message : 'JSON sent is invalid.'
+      });
+    } else {
+      next();
+    }
+  });
+  
+  app.use(helper.parseQueryString);
 
   // Bind the api routes.
   loadApi(app);
@@ -111,7 +61,7 @@ if (config.express.isProduction && cluster.isMaster) {
       logger.logAppErrors(error);
       process.exit(10);
     }
-    logger.logAppInfo("express is listening on http://" + config.express.ip
+    logger.logAppInfo('Express is listening on http://' + config.express.ip
             + ":" + config.express.port);
   });
 };
