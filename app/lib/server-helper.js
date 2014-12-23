a871cc96b663a2b39e186da9e6c03a9f575e335e
Abijeet <abijeet.p@osmosys.asia>
2014-12-23 21:13:11 +0530

    Update server helper to handle admin users.

--- app/lib/server-helper.js
+++ app/lib/server-helper.js
@@ -3,39 +3,57 @@ var loadApi = require(__CONFIG__.app_code_path +'api.js');
 
 var serverHelper = function() {
   var app = null;
-
+  var validMethodTypes = ['get', 'post', 'delete', 'put'];
   var init = function(baseApp) {
     app = baseApp;
-    app.httpPost = function(url, route, isPublic) {
-      bindHttpRequest(getFinalUrl(url, isPublic), route, isPublic, 'post');
+    app.httpPost = function(url, route, isPublic, isAdmin) {
+      bindRequest(url, route, isAdmin, isPublic, 'post');      
     };
 
-    app.httpGet = function(url, route, isPublic) {
-      bindHttpRequest(getFinalUrl(url, isPublic), route, isPublic, 'get');
+    app.httpGet = function(url, route, isPublic, isAdmin) {
+      bindRequest(url, route, isAdmin, isPublic, 'get');      
     };
 
-    app.httpDelete = function(url, route, isPublic) {
-      bindHttpRequest(getFinalUrl(url, isPublic), route, isPublic, 'delete');
+    app.httpDelete = function(url, route, isPublic, isAdmin) {
+      bindRequest(url, route, isAdmin, isPublic, 'delete');      
+    };
+    
+    app.httpPut = function(url, route, isPublic, isAdmin) {
+      bindRequest(url, route, isAdmin, isPublic, 'put');
     };
   };
 
-  var bindHttpRequest = function(url, route, isPublic, method) {
-    console.log(url);
+  var bindRequest = function(url, route, isAdmin, isPublic, methodType) {
+    url = normalizeUrl(url);
+    methodType = methodType.toLowerCase();
+    if(isAdmin) {     
+      isPublic = false;
+    }
+    if(validMethodTypes.indexOf(methodType) !== -1) {
+      bindHttpRequest(getFinalUrl(url, isPublic), route, isPublic, isAdmin, methodType);
+    } else {
+      console.log('Invalid method type for - ' + url);
+    }
+  };
+  
+  var bindHttpRequest = function(url, route, isPublic, isAdmin, method) {
     if (isPublic) {
       app[method](url, route);
     } else {
       app[method](url, loadApi.validate);
+      if(isAdmin) {
+        app[method](url, loadApi.checkIfAdmin);
+      }
       app[method](url, route);
     }
   };
 
-  var normalizeUrl = function(url) {
-    if (url.indexOf('/') === 0) { return url; }
+  var normalizeUrl = function(url) {   
+    if (url.indexOf('/') === 0 || url.lastIndexOf('/') === url.length - 1) { return url.replace(/^\/|\/$/g, ''); }
     return url;
   };
-
-  var getFinalUrl = function(url, isPublic) {
-    url = normalizeUrl(url);
+  
+  var getFinalUrl = function(url, isPublic) {    
     var finalUrl = __CONFIG__.app_base_url_token + url;
     if (isPublic) {
       finalUrl = __CONFIG__.app_base_url + url;
