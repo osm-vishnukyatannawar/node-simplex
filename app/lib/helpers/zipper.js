37f27052d9d17f135687d817b052ef43b08e74e0
Abijeet <abijeet.p@osmosys.asia>
2015-01-09 16:03:12 +0530

    Now returning AppError from zipper helper.

--- app/lib/helpers/zipper.js
+++ app/lib/helpers/zipper.js
@@ -1,31 +1,28 @@
  var admzip = require('adm-zip');
  var getStatus = require(__CONFIG__.app_base_path + 'lib/status');
- 
- function zipper(){
-	 
+
+ function zipper() {
+
  };
- 
- zipper.prototype.getFilesInfo = function(sourcePath , cb){
-	try{
-	var zip = new admzip(sourcePath);
-	var zipEntries = zip.getEntries();
-	}catch(e){
-		return cb(e);
-	}
-	//zip.extractAllTo( destPath, true);
-	return cb(null,zipEntries);
-};
- 
- zipper.prototype.extractFiles = function(sourcePath , destpath , cb){
-	 try{
-		 var zip = new admzip(sourcePath);
-		 var result =  zip.extractAllTo(destpath, true);
-		 return cb(null);
-	 }catch(e){
-		 return cb(e); 
-	 }	 	 
+
+ zipper.prototype.getFilesInfo = function(sourcePath, cb) {
+   try {
+     var zip = new admzip(sourcePath);
+     var zipEntries = zip.getEntries();
+   } catch (e) {
+     return cb(new AppError(e, 'There was an error while reading the zip file.', {}));
+   }
+   return cb(null, zipEntries);
  };
- 
- 
- 
+
+ zipper.prototype.extractFiles = function(sourcePath, destpath, cb) {
+   try {
+     var zip = new admzip(sourcePath);
+     zip.extractAllTo(destpath, true);
+     return cb(null);
+   } catch (e) {
+     return cb(new AppError(e, 'There was an error while extracting the zip file.', {}));
+   }
+ };
+
  module.exports = zipper;
