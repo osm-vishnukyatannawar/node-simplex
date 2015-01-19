0a76743efadf77e61cde501b8e7e8d41b4e2c108
Abijeet <abijeet.p@osmosys.asia>
2015-01-19 13:36:59 +0530

    Adding value in config.js - maxDecimalLength - used to round off decimals.

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 var config = module.exports;
-var PRODUCTION = process.env.NODE_ENV === "production";
+var PRODUCTION = process.env.NODE_ENV === 'production';
 global.__CONFIG__ = {
   'app_base_path': __dirname + '/',
   'app_code_path': __dirname + '/code/',
@@ -22,8 +22,7 @@ global.__CONFIG__ = {
     'max_tries': 2,
     'default_value_tag_sn': '999',
     'default_value_org': '999',
-    'default_value_type': 99,
-    'max_tries': 2
+    'default_value_type': 99    
   },
   'user': {
     'default_password': 'cb8da6a0-776f-4f2e-acba-9055b7bcb3a5',
@@ -39,27 +38,28 @@ global.__CONFIG__ = {
       'wifi_fw_version': 'PowerPath_WIFI_FW_',
       'host_fw_version': 'PowerPath_HOST_FW_'
     },
-    'firmwareFileExtension': ".zip",
-    'baseVersionFolderName' : "Powerpath_FW_version_"
+    'firmwareFileExtension': '.zip',
+    'baseVersionFolderName' : 'Powerpath_FW_version_'
   },
   'calibration' :{
-	  'allRecordsCsvFileName' : "allRows.csv",
-	  'failureRecordsFileName' : "failureRecords.csv",
-	  'exportCsvFileDownloadName' : "EmanateDevices.csv",
-	  'unExportedLogFileName' : "UnexportedDevicesLog.csv"
+	  'allRecordsCsvFileName' : 'allRows.csv',
+	  'failureRecordsFileName' : 'failureRecords.csv',
+	  'exportCsvFileDownloadName' : 'EmanateDevices.csv',
+	  'unExportedLogFileName' : 'UnexportedDevicesLog.csv'
   },
   'lookup' : {
 	'powerPathAssetType' : 'powerPathAssetType'  
   },
   'tokenLength': 16,
   'fwTokenLength': 16,
-  'firmwareFileExtension': ".zip",
-  'filesFolderName' : "files",
+  'firmwareFileExtension': '.zip',
+  'filesFolderName' : 'files',
+  'maxDecimalLength' : 2,
   'logs' : [{key:'1',value:'dev_col_import'},{key:'2',value:'dev_col_export'}]
 };
 
 config.express = {
   port: process.env.EXPRESS_PORT || 3000,
-  ip: "10.0.0.15",
+  ip: '10.0.0.15',
   isProduction: PRODUCTION
 };
