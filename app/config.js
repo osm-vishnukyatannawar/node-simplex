05defe98bd13de4252002f9474723b4b36eb48a6
Mark Craig <mcraig@emanatewireless.com>
2015-08-26 12:57:02 -0700

    Adds the 'EMANATE_HTTP_PORT' and 'EMANATE_HTTPS_PORT' environment variables for configuring the HTTP and HTTPS server ports (defaults to 3000, 3001).

--- app/config.js
+++ app/config.js
@@ -10,6 +10,8 @@ var PRODUCTION = process.env.NODE_ENV;
 var isStaging = process.env.NODE_ENV_STAGING;
 var logDir = process.env.EMANATE_LOG_DIR || (__dirname + '/../logs/');
 var networkInterfaceName = process.env.EMANATE_NETWORK_INTERFACE || 'eth0';
+var port = process.env.EMANATE_HTTP_PORT || 3001;
+var httpsPort = process.env.EMANATE_HTTPS_PORT || 3002;
 
 // get the ip-address associated with the configured network interface name
 var ipAddress = networkUtils.getIpAddressForNetworkInterface(networkInterfaceName) || '127.0.0.1';
@@ -26,8 +28,6 @@ var isHttps = true;
 var emailsToSend = 'surendra.b@osmosys.asia';
 var debugSupportMails  = 'surendra.b@osmosys.asia';
 
-var port = 3001;
-var httpsPort = 3002;
 var slogerrAppID = '551a6f48-e2c4-45aa-80e5-1de45a0bc003';
 
 //
